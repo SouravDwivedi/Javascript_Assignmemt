c409ed76ffa7019d86132a4ee6d73b20a2a9bf0b
SouravDwivedi <136299174+SouravDwivedi@users.noreply.github.com>
2023-08-16 23:55:32 +0530

    Add files via upload

---
+++ Chapter 9/export.js
@@ -0,0 +1,17 @@
+// export const name="Sourav";
+// export function myname(){
+// return `hello,${name}`;
+// }
+// export class Emp{
+//     constructor(){
+//         console.log("this is called constructor");
+//     }
+// }
+
+// export{name,myname,Emp}; You can use this as well 
+
+
+// default export
+ export default function(){
+    console.log("this is default method which doesn't have name")
+ }
---
+++ Chapter 9/import.js
@@ -0,0 +1,19 @@
+// import{name,myname,Emp} from './export.js';
+// document.body.innerHTML = name;
+// document.body.innerHTML = myname();
+// const emp=new Emp();
+
+// using (*) import everything 
+// import * as sd from './export.js';
+// document.body.innerHTML = sd.name;
+// document.body.innerHTML = sd.myname();
+// const emp=new sd.Emp();
+
+// default export ( 1st way )
+// import {default as value} from './export.js';
+// value(); 
+
+
+// default export ( 2nd  way )
+import value from './export.js';
+value();  
